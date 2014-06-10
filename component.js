ee52669d885de729c543518c539a1cc1f1ceb89f
Karl Podger <karl@wednesdayagency.com>
2014-06-10 23:38:12 +0100

    Register element with Polymer

--- component.js
+++ component.js
@@ -1,3 +1,14 @@
-window.addEventListener('WebComponentsReady', function() {
-    console.log('element-navigation registered');
+Polymer('element-navigation', {
+    created: function() {
+        console.log('element-navigation created');
+    },
+    enteredView: function() {
+        console.log('element-navigation enteredView');
+    },
+    leftView: function() {
+        console.log('element-navigation leftView');
+    },
+    attributeChanged: function(attrName, oldVal, newVal) {
+        console.log('element-navigation attributeChanged');
+    }
 });
